3b944aa7cabea12d61b1e797abce93b51a101d9c
Darius Walker <dw862716@ohio.edu>
2020-07-15 20:50:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,14 @@
 # Make your shoe class here!
 class Shoe 
 attr_accessor :brand, :color, :size, :material, :condition
-
+attr_reader :repaired 
 
 
  def initialize(brand)
     @brand=brand
  end 
  
- 
+
  
  
 end 
