ceb99cd80a5958a28586529a96ac0c7a9009816c
Darius Walker <dw862716@ohio.edu>
2020-07-15 20:54:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,9 +8,10 @@ attr_accessor :brand, :color, :size, :material, :condition
     @brand=brand
  end 
  
- def cobble 
-   puts "Your shoe is as good as new!"
+ def cobble
    condition = "new"
+   puts "Your shoe is as good as new!"
+
  end 
  
  
