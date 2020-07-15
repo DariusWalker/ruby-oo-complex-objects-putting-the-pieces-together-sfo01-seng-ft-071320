43d3566688338b076702abcc6ad62aa02828f0ea
Darius Walker <dw862716@ohio.edu>
2020-07-15 20:38:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,11 @@
 # Make your shoe class here!
 class Shoe 
 attr_accessor :shoe, :brand 
+ 
  def initialize(shoe, brand)
    @shoe=shoe 
    @brand=brand
  end 
- def shoe
-   @shoe
- end 
- def brand
-   @brand
- end 
+ 
  
 end 
